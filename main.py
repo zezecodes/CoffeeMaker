b1b6d0763966098349c9657c0b1996f27fb62139
zezecodes <djangsiness01@gmail.com>
2022-02-06 13:56:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from menu import Menu, MenuItem
+from coffee_maker import coffee_maker 
+from money_machine import money_machine
+
+machine = MoneyMachine()
+maker = CoffeeMaker()
+menu = Menu()
+
+machine_on = True
+
+machine.report()
+maker.report()
+
+while machine_on:
+    options = menu.get_items()
+    order = input(f"What would you like to order? - ({options}) : ")
+    if order == "off":
+        machine_on = False
+    elif order == "report":
+        maker.report()
+        machine.report()
+    else:
+        drink = menu.find_drink(order)
+        is_enough_ingredients = maker.is_resource_sufficient(drink)
+        is_payment_successful = machine.make_payment(drink.cost)
+        if is_enough_ingredients and is_payment_successful:
+            maker.make_coffee(drink) 
